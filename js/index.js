117ddc8f795dc320b352ce0337e3842f64819f76
Hlaing Ei Tun <zwekh12@example.com>
2025-05-22 22:27:08 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -20,3 +20,45 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+// Select the form by name
+const messageForm = document.forms["leave_message"];
+
+// Add event listener to handle form submission
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submit behavior
+
+  const usersName = event.target.usersName.value;
+  const usersEmail = event.target.usersEmail.value;
+  const usersMessage = event.target.usersMessage.value;
+
+  // Log the values to the console
+  console.log("Name:", usersName);
+  console.log("Email:", usersEmail);
+  console.log("Message:", usersMessage);
+
+const messageSection = document.querySelector("#messages");
+
+const messageList = messageSection.querySelector("ul");
+
+const newMessage = document.createElement("li");
+
+newMessage.innerHTML = `
+  <a href="mailto:${usersEmail}">${usersName}</a>
+  <span> wrote: ${usersMessage}</span>
+`;
+
+const removeButton = document.createElement("button");
+removeButton.innerText = "remove";
+removeButton.type = "button";
+
+removeButton.addEventListener("click", function () {
+  const entry = removeButton.parentNode;
+  entry.remove();
+});
+
+newMessage.appendChild(removeButton);
+messageList.appendChild(newMessage);
+
+messageForm.reset();
+
+});
